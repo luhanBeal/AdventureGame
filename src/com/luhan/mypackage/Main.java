b3c7489fd7d206b0b4be480a35aab604b5d34849
luhan <luhansantanna@gmail.com>
2020-05-06 18:12:12 -0300

    Added exits to the game

--- src/com/luhan/mypackage/Main.java
+++ src/com/luhan/mypackage/Main.java
@@ -17,15 +17,44 @@ public class Main {
         locations.put(4, new Location(4, "You are in the backyard stargazing."));
         locations.put(5, new Location(5, "You are in your parents bedroom looking at your phone."));
 
+        //adding exits
+        locations.get(1).addExit("w", 5);
+        locations.get(1).addExit("S", 2);
+        locations.get(1).addExit("N", 3);
+        locations.get(1).addExit("Q", 0);
+
+        locations.get(2).addExit("N", 1);
+        locations.get(2).addExit("Q", 0);
+
+        locations.get(3).addExit("N", 4);
+        locations.get(3).addExit("S", 1);
+        locations.get(3).addExit("Q", 0);
+
+        locations.get(4).addExit("S", 3);
+        locations.get(4).addExit("Q", 0);
+
+        locations.get(5).addExit("E", 1);
+        locations.get(5).addExit("Q", 0);
+
         int loc =1;
         while (true) {
             System.out.println(locations.get(loc).getDescription());
             if (loc == 0) {
                 break;
             }
-            loc = scanner.nextInt();
-            if (!locations.containsKey(loc)) {
-                System.out.println("You cannot go that way");
+
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.print("You can go: ");
+            for (String exit: exits.keySet()) {
+                System.out.println(exit + ", ");
+            }
+            System.out.println();
+
+            String direction = scanner.nextLine().toUpperCase();
+            if (exits.containsKey(direction)) {
+                loc = exits.get(direction);
+            } else {
+                System.out.println("You cannot go in that direction.");
             }
         }
     }
